2e0a4dacbf0d55c9f37e7e1b8baeb72c954d4fe9
Tahinli <Kamuflaj@protonmail.com>
2022-06-05 15:50:53 +0300

    a
    a

--- WildBee/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.wildbee.wildbee {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires AnimateFX;
-
-
-    opens com.wildbee.wildbee to javafx.fxml;
-    exports com.wildbee.wildbee;
-}
