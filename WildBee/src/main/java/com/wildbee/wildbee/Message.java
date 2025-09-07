d5fd2d78172d24cdaf8c4c5264d04a04974ea328
Ahmet Kaan Gümüş <tahinli@tahinli.com>
2025-09-07 11:32:05 +0300

    Net Final
    Net Final

---
+++ WildBee/src/main/java/com/wildbee/wildbee/Message.java
@@ -0,0 +1,14 @@
+package com.wildbee.wildbee;
+
+/**
+ * The class to which the message is passed.
+ * @since 2022-06
+ */
+class Message {
+    int point;
+    String word;
+    String game;
+    boolean isFinished;
+    String finish;
+
+}
